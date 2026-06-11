ef3e8e98e3c1b6ea1c870433e268015ac3f96fa6
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-06-10 21:46:06 -0700

    haveSameShape solved

--- src/Traversals.java
+++ src/Traversals.java
@@ -153,7 +153,13 @@ public class Traversals {
    * @return true if the trees have the same shape, false otherwise
    */
   public static <T> boolean haveSameShape(TreeNode<T> nodeA, TreeNode<T> nodeB) {
-    return false;
+    if (nodeA == null && nodeB == null) {
+        return true;
+    }
+    if (nodeA == null || nodeB == null) {
+        return false;
+    }
+    return haveSameShape(nodeA.left, nodeB.left) && haveSameShape(nodeA.right, nodeB.right);
   }
 
 
